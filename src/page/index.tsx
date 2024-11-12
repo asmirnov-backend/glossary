5f66947c23c3e88151d7e98ceb38913076aa386b
Andrew Smirnov <smirnov.andrew.1999@yandex.ru>
2024-11-12 19:10:33 +0300

    fix

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -38,15 +38,6 @@ export const Page: React.FC = () => {
     };
   }, []);
 
-  const scrollToTop = () => {
-    const top = document.documentElement.scrollTop || document.body.scrollTop;
-
-    if (top > 0) {
-      window.requestAnimationFrame(scrollToTop);
-      window.scrollTo(0, top - top / 8);
-    }
-  };
-
   return (
     <div className="portfolio-area">
       <Header />
